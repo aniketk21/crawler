58824958d8cc7a251e5ac9124794fd4471fedf40
Aniket Kulkarni <kaniket21@gmail.com>
2017-11-09 20:56:38 +0530

    Add pdf_extract.py

---
+++ pdf_extract.py
@@ -0,0 +1,38 @@
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+def download_file(url, req_sess):
+    local_filename = url.split('/')[-1]
+    #print(local_filename)
+    r = req_sess.get(url, stream=True, timeout=10)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+    return local_filename
+
+def convert(fname, pages=None):
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = open(fname, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close
+    return text
+
+def pdf_caller(url, req_sess):
+    return convert(download_file(url, req_sess))
